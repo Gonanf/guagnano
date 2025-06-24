19fd44414bdca37afeef20455fe7343cceb53ce4
Chaos <gabysolotorevsky100@gmail.com>
2025-06-23 21:52:48 -0300

    Sending

---
+++ build/CMakeFiles/tarea.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea.dir/link.d"
+  "CMakeFiles/tarea.dir/src/main.cpp.o"
+  "CMakeFiles/tarea.dir/src/main.cpp.o.d"
+  "tarea"
+  "tarea.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
