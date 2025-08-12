3e12d447afc95aaadb503a2e3c5fd720c49af9ec
Gabriel <gabysolotorevsky100@gmail.com>
2025-08-11 23:58:28 -0300

    Refactoring and added deletion and migration into a deleted students table

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <iostream>
-#include"sqlite3.h"
+#include "sqlite3.h"
 
 /**@main */
 
@@ -22,9 +22,11 @@
  * Incorporar conexion a bases de datos (Actividad actual)
  ***************************************/
 
-int callback(void* param, int argc, char** argv, char** col){
+int callback(void *param, int argc, char **argv, char **col)
+{
 	std::cout << "------------------------------" << std::endl;
-	for (int i = 0; i < argc; i++){
+	for (int i = 0; i < argc; i++)
+	{
 		std::cout << col[i] << " : " << argv[i] << std::endl;
 	}
 	std::cout << "------------------------------" << std::endl;
@@ -54,18 +56,24 @@ bool CIN_FAILSAFE()
 
 #define MAX_CHARS 25
 
-int add_alumno(sqlite3* db)
+/**
+ * @brief Funcion para guiar al usuario en el proceso de la insercion de un alumno nuevo
+ * 
+ * @param db La base de datos al cual se le haran las modificaciones
+ * @return int Devuelve 0 si no hay problemas, devuelve -1 si hubo un error
+ */
+int add_alumno(sqlite3 *db)
 {
-	struct{
-char nombre[MAX_CHARS];
-	char apellido[MAX_CHARS];
-	int edad;
-	int materias_aprovadas;
-	char estado_civil;
-	char sexo;
+	struct
+	{
+		char nombre[MAX_CHARS];
+		char apellido[MAX_CHARS];
+		int edad;
+		int materias_aprovadas;
+		char estado_civil;
+		char sexo;
 	} alumno;
-	
-	
+
 	do
 	{
 		std::cout << "Ingresar nombre" << std::endl;
@@ -110,37 +118,109 @@ char nombre[MAX_CHARS];
 
 		std::cin >> alumno.materias_aprovadas;
 	} while (CIN_FAILSAFE());
-	sqlite3_stmt* statement;
-	sqlite3_prepare_v2(db,"INSERT INTO alumnos(nombre,apellido,edad,materias_aprovadas,estado_civil,sexo) VALUES(?,?,?,?,?,?);",-1,&statement,NULL);
-	sqlite3_bind_text(statement,1,alumno.nombre,25,SQLITE_STATIC);
-	sqlite3_bind_text(statement,2,alumno.apellido,25,SQLITE_STATIC);
-	sqlite3_bind_int(statement,3,alumno.edad);
-	sqlite3_bind_int(statement,4,alumno.materias_aprovadas);
-	sqlite3_bind_text(statement,5,&alumno.estado_civil,1,SQLITE_STATIC);
-	sqlite3_bind_text(statement,6,&alumno.sexo,1,SQLITE_STATIC);
+	sqlite3_stmt *statement;
+	sqlite3_prepare_v2(db, "INSERT INTO alumnos(id_al,nombre,apellido,edad,materias_aprovadas,estado_civil,sexo) SELECT " //! Se utiliza SELECT en vez de VALUES por que SELECT permite hacer consultas dentro de los parametros
+	"(SELECT MAX(id) + 1 FROM (SELECT MAX(id_al) AS id FROM alumnos UNION ALL SELECT MAX(id_reg) as id FROM alumnos_reg))" //! Selecciona la mayor ID posible del mayor ID de alumnos o de la tabla de eliminados
+	",?,?,?,?,?,?;", -1, &statement, NULL);
+	sqlite3_bind_text(statement, 1, alumno.nombre, 25, SQLITE_STATIC);
+	sqlite3_bind_text(statement, 2, alumno.apellido, 25, SQLITE_STATIC);
+	sqlite3_bind_int(statement, 3, alumno.edad);
+	sqlite3_bind_int(statement, 4, alumno.materias_aprovadas);
+	sqlite3_bind_text(statement, 5, &alumno.estado_civil, 1, SQLITE_STATIC);
+	sqlite3_bind_text(statement, 6, &alumno.sexo, 1, SQLITE_STATIC);
 
-	if (sqlite3_step(statement) != SQLITE_DONE){
-		std::cerr << "Error inserting alumnos: " << std::endl <<" "<< sqlite3_errmsg(db) << std::endl;
+	if (sqlite3_step(statement) != SQLITE_DONE)
+	{
+		std::cerr << "Error inserting alumnos: " << std::endl
+				  << " " << sqlite3_errmsg(db) << std::endl;
 		return -1;
 	}
 	return 0;
 }
 
+/**
+ * @brief Funcion para guiar al usuario a eliminar un alumno
+ * 
+ * @param db La base de datos al cual se le haran las modificaciones
+ * @return int Devuelve 0 si no hay problemas, devuelve -1 si hubo un error
+ */
+int remove_alumno(sqlite3 *db)
+{
+	int id;
+	char *errmsg;
+	if (sqlite3_exec(db, "SELECT * FROM alumnos;", callback, NULL, &errmsg) != 0)
+	{
+		std::cerr << "Error selecting from alumnos: " << std::endl
+				  << " " << errmsg << std::endl;
+		return -1;
+	}
+	do
+	{
+		std::cout << "Ingresar la ID del alumno a eliminar: ";
+		std::cin >> id;
+	} while (CIN_FAILSAFE());
+
+	sqlite3_stmt *statement;
+	// Migracion, se inserta en la tabla de registros de alumnos retirados utilizando todos los datos del registro de la tabla alumnos que tenga la ID especificada
+	sqlite3_prepare_v2(db, "INSERT INTO alumnos_reg SELECT id_al,nombre,apellido,edad,materias_aprovadas,estado_civil,sexo  FROM alumnos WHERE id_al = ?;", -1, &statement, NULL);
+	sqlite3_bind_int(statement, 1, id);
+	sqlite3_bind_int(statement, 2, id);
+	if (sqlite3_step(statement) != SQLITE_DONE)
+	{
+		std::cerr << "Error inserting alumnos_reg: " << std::endl
+				  << " " << sqlite3_errmsg(db) << std::endl;
+		return -1;
+	}
+	if (sqlite3_reset(statement) != SQLITE_OK)
+	{
+		std::cerr << "Error reseting the statement: " << std::endl
+				  << " " << sqlite3_errmsg(db) << std::endl;
+		return -1;
+	}
+
+	sqlite3_prepare_v2(db, "DELETE FROM alumnos WHERE id_al = ?;", -1, &statement, NULL);
+	sqlite3_bind_int(statement, 1, id);
+
+	if (sqlite3_step(statement) != SQLITE_DONE)
+	{
+		std::cerr << "Error deleting alumno: " << std::endl
+				  << " " << sqlite3_errmsg(db) << std::endl;
+		return -1;
+	}
+	if (sqlite3_finalize(statement) != SQLITE_OK)
+	{
+		std::cerr << "Error finalizing the statement: " << std::endl
+				  << " " << sqlite3_errmsg(db) << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
+
 int main()
 {
-	system("clear");
-	sqlite3* db;
+	sqlite3 *db;
 
-
-	if (sqlite3_open("amongas.db",&db)){
+	if (sqlite3_open("amongas.db", &db))
+	{
 		std::cerr << "Error opening the database" << std::endl;
 		return -1;
 	}
 
-	char* errmsg;
+	char *errmsg;
 
-	if (sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS alumnos (id INTEGER PRIMARY KEY,nombre TEXT, apellido TEXT, edad INTEGER, materias_aprovadas INTEGER, estado_civil varchar(1),sexo varchar(1));",NULL,NULL,&errmsg) != 0){
-		std::cerr << "Error creating a table alumnos: " << std::endl <<" "<<errmsg << std::endl;
+	if (sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS alumnos (id_al INTEGER PRIMARY KEY,nombre TEXT, apellido TEXT, edad INTEGER, materias_aprovadas INTEGER, estado_civil varchar(1),sexo varchar(1));", NULL, NULL, &errmsg) != 0)
+	{
+		std::cerr << "Error creating a table alumnos: " << std::endl
+				  << " " << errmsg << std::endl;
+		return -1;
+	}
+
+	// En esta situacion añadiria una fecha de eliminacion, pero no me quiero adentrar mucho en el tema de los NTP
+	if (sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS alumnos_reg (id_reg INTEGER PRIMARY KEY,nombre TEXT, apellido TEXT, edad INTEGER, materias_aprovadas INTEGER, estado_civil varchar(1),sexo varchar(1));", NULL, NULL, &errmsg) != 0)
+	{
+		std::cerr << "Error creating a table alumnos_reg: " << std::endl
+				  << " " << errmsg << std::endl;
 		return -1;
 	}
 
@@ -148,7 +228,14 @@ int main()
 	while (true)
 	{
 		int menu;
-		WAIT_VALID_INPUT("----------\n-Ingresa 1 para añadir un alumno\n-Ingresa 2 para ver el listado de alumnos\n-Ingresa 3 para ver el listado de nombres y apellidos\n-Ingresa 4 para obtener el promedio de edad\n-Ingresa 5 para salir\n----------", 1, 5, menu);
+		WAIT_VALID_INPUT("----------\
+		\n-Ingresa 1 para añadir un alumno \
+		\n-Ingresa 2 para ver el listado de alumnos\n-Ingresa 3 para ver el listado de nombres y apellidos\
+		\n-Ingresa 4 para obtener el promedio de edad\
+		\n-Ingresa 5 para eliminar un alumno\
+		\n-Ingresa 6 para ver el listado de alumnos eliminados\
+		\n-Ingresa 7 para salir\
+		\n----------", 1, 7, menu);
 		system("clear");
 		switch (menu)
 		{
@@ -158,25 +245,42 @@ int main()
 		}
 		break;
 		case 2:
-			if (sqlite3_exec(db,"SELECT * FROM alumnos;",callback,NULL,&errmsg) != 0){
-					std::cerr << "Error selecting from alumnos: " << std::endl <<" "<<errmsg << std::endl;
-					return -1;
-				}
+			if (sqlite3_exec(db, "SELECT * FROM alumnos;", callback, NULL, &errmsg) != 0)
+			{
+				std::cerr << "Error selecting from alumnos: " << std::endl
+						  << " " << errmsg << std::endl;
+				return -1;
+			}
 			break;
 
 		case 3:
-			if (sqlite3_exec(db,"SELECT nombre, apellido FROM alumnos;",callback,NULL,&errmsg) != 0){
-					std::cerr << "Error selecting from alumnos: " << std::endl <<" "<<errmsg << std::endl;
-					return -1;
-				}
+			if (sqlite3_exec(db, "SELECT nombre, apellido FROM alumnos;", callback, NULL, &errmsg) != 0)
+			{
+				std::cerr << "Error selecting from alumnos: " << std::endl
+						  << " " << errmsg << std::endl;
+				return -1;
+			}
 			break;
 		case 4:
-			if (sqlite3_exec(db,"SELECT avg(edad) FROM alumnos;",callback,NULL,&errmsg) != 0){
-					std::cerr << "Error selecting from alumnos: " << std::endl <<" "<<errmsg << std::endl;
-					return -1;
-				}
+			if (sqlite3_exec(db, "SELECT avg(edad) FROM alumnos;", callback, NULL, &errmsg) != 0)
+			{
+				std::cerr << "Error selecting from alumnos: " << std::endl
+						  << " " << errmsg << std::endl;
+				return -1;
+			}
 			break;
 		case 5:
+			remove_alumno(db);
+			break;
+		case 6:
+			if (sqlite3_exec(db, "SELECT * FROM alumnos_reg;", callback, NULL, &errmsg) != 0)
+			{
+				std::cerr << "Error selecting from alumnos_reg: " << std::endl
+						  << " " << errmsg << std::endl;
+				return -1;
+			}
+			break;
+		case 7:
 			return 0;
 			break;
 		}
